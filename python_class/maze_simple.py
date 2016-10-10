66e5a3113135f3ee9f164c827260a5da86f5f6f3
Michael Bubb <mbubb@forensiq.com>
2016-10-10 12:24:03 -0400

    maze example

---
+++ python_class/maze_simple.py
@@ -0,0 +1,22 @@
+import turtle
+
+t = turtle.Pen()
+
+######################
+# draw maze
+#
+t.left(135)
+t.up()
+t.forward(400)
+t.right(135)
+t.down()
+t.forward(520)
+t.right(90)
+t.forward(520)
+t.color('red')
+#########################
+#solution
+#
+t.forward(500)
+t.right(90)
+t.forward(80)
