91139eadc4a090ab558345815d845f8719a94410
Michael Bubb <mbubb@forensiq.com>
2016-10-18 15:21:28 -0400

    left shift

--- pyscriptcheck.sh
+++ pyscriptcheck.sh
@@ -4,5 +4,5 @@ FILEPATH=$(pwd)
 find ${FILEPATH} -iname "*.pyc" -exec rm {} \;
 python -m compileall -q ${FILEPATH}
 find ${FILEPATH} -iname "*.pyc" -exec rm {} \;
-python3 -m compileall -q ${FILEPATH}
-find ${FILEPATH} -iname "*.pyc" -exec rm {} \;
+#python3 -m compileall -q ${FILEPATH}
+#find ${FILEPATH} -iname "*.pyc" -exec rm {} \;
