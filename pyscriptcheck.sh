55b24207e4eab5e61edce498079559b267be5c22
Michael Bubb <mbubb@forensiq.com>
2016-10-18 16:26:31 -0400

    introduce a deliberate err for travis

--- pyscriptcheck.sh
+++ pyscriptcheck.sh
@@ -3,6 +3,6 @@
 FILEPATH=$(pwd)
 find ${FILEPATH} -iname "*.pyc" -exec rm {} \;
 python -m compileall -q ${FILEPATH}
-compile_result =  $?
+compile_result=$?
 find ${FILEPATH} -iname "*.pyc" -exec rm {} \;
 exit ${compile_result}
