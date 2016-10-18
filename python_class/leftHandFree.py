7edc6ca9cd2d709b93644cc9759600c4e16bab5f
Michael Bubb <mbubb@forensiq.com>
2016-10-18 14:32:06 -0400

    left shift

---
+++ python_class/leftHandFree.py
@@ -0,0 +1,12 @@
+n = 5
+k = 4
+a = [1,2,3,4,5]
+def left_shift(n,k,a):
+   for _ in range(k):
+      tmpa = a.pop(0)
+      a.append(tmpa)
+   print(*a)
+
+left_shift(n,k,a)
+#answer = left_shift(n,k,a);
+#print(*answer, sep=' ')
---
+++ python_class/leftHandFree2.py
@@ -0,0 +1,19 @@
+import string
+
+n = 35
+k = 5
+#a = []
+a = list(string.ascii_letters)
+
+def load_array(n,k):
+  for i in range(n):
+     a.append(i+1)
+   
+def left_shift(k,a):
+   for _ in range(k):
+      a.append(a.pop(0))
+   print(*a)
+
+#load_array(n,k)
+left_shift(k,a)
+
