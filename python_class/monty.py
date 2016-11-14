0abf1e7eaea0dec1a5eeb7d11919fd9e1a82241b
Michael Bubb <mbubb@forensiq.com>
2016-11-14 11:28:28 -0500

    random and tide exercise

---
+++ python_class/monty.py
@@ -0,0 +1,34 @@
+from random import *
+from turtle import *
+
+num = 10
+a = 3
+z = 10000
+
+#def give_me_random_nums(start_range, end_range):
+#    return randint(start_range, end_range)
+          
+#for _ in range(num):
+#    print(give_me_random_nums(a, z))
+
+
+#color_list = ["blue", "green", "yellow", "orange", "red", "purple", "gray", "brown"]
+
+#for i in range(len(color_list)):
+#    print(color_list[i])
+
+#lownum = 1
+#highnum = 20
+
+#the_number = randint(lownum, highnum)
+#guess = int(input("Guess a number between " + str(lownum) + " and " + str(highnum) + ": " ))
+
+#while guess != the_number:
+#    if guess > the_number:
+#        print(guess, "was too high. Try again.")
+#    if guess < the_number:
+#        print(guess, "was too low. Try again.")
+#    guess = int(input("Guess again: "))
+
+#print(guess, "was the number! You win!")
+    
---
+++ python_class/rando.py
@@ -0,0 +1,55 @@
+from random import *
+from turtle import *
+
+###  Random Numbers
+###  find me at http://mbubb.devio.us/python_class/rando.py
+###  run me at http://www.pythonide.org/run.html
+###  confused? check the docs at https://docs.python.org/2/
+###  or email
+###  michael.bubb@gmail.com
+
+
+num = 10
+a = 3
+z = 10000
+
+### 
+### Function to give out random numbers
+### 
+#def give_me_random_nums(start_range, end_range):
+#    return randint(start_range, end_range)
+
+### 
+### Use the funcion in a loop
+### 
+#for _ in range(num):
+#    print(give_me_random_nums(a, z))
+
+### 
+### Use the color list to print random colors
+###
+
+#color_list = ["blue", "green", "yellow", "orange", "red", "purple", "gray", "brown"]
+
+
+### 
+### Guessing game
+### 
+
+#lownum = 1
+#highnum = 20
+
+#the_number = randint(lownum, highnum)
+#guess = int(input("Guess a number between " + str(lownum) + " and " + str(highnum) + ": " ))
+
+#while guess != the_number:
+#    if guess > the_number:
+#        print(guess, "was too high. Try again.")
+#    if guess < the_number:
+#        print(guess, "was too low. Try again.")
+#    guess = int(input("Guess again: "))
+
+#print(guess, "was the number! You win!")
+
+
+    
