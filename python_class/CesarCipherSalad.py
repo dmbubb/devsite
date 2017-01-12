ab2d2d4df6395af77204e76f9072ca648f00ed78
Michael Bubb <mbubb@forensiq.com>
2017-01-12 12:45:51 -0500

    first clue

--- python_class/CesarCipherSalad.py
+++ python_class/CesarCipherSalad.py
@@ -1,5 +1,6 @@
 from codecs import *
 from string import *
+from math import *
 
 testr = "The quick brown fox jumped over the lazy dog"
 #testr = str(input("string to decode: "))
@@ -8,9 +9,19 @@ srcList = ascii_uppercase
 #srcList = ascii_letters
 #srcList = printable
 
-
+tstr2 = 'GUR DHVPX OEBJA SBK WHZCRQ BIRE GUR YNML QBT'
 tstr = testr.upper()
 
+def caesarShift(offset):
+    cshift = ''
+    orig = ''
+    for i in range(len(ascii_uppercase)):
+        newind = (i + offset) % len(ascii_uppercase)
+        orig = orig + " | " + ascii_uppercase[i]
+        cshift = cshift + " | " + ascii_uppercase[newind]
+    print("offset is ", offset)
+    print(orig)
+    print(cshift)
 
 def rotTrans(myStr,offset):
     newstr = ''
@@ -27,12 +38,12 @@ def rotTrans(myStr,offset):
 
 for i in range(len(srcList)):
     print("ROT ",i)
-    print(rotTrans(tstr,i))
+    print(rotTrans(tstr2,i))
 
-offs = int(len(srcList)/2)
-print(offs)
-print("original string ", tstr)
-firstStr = rotTrans(tstr,offs)
-print("first transform ",firstStr)
-print("return to orig ",rotTrans(firstStr,offs))
+#offs = int(len(srcList)/2)
+#print(offs)
+#print("original string ", tstr)
+#firstStr = rotTrans(tstr,offs)
+#print("first transform ",firstStr)
+#print("return to orig ",rotTrans(firstStr,offs))
 
